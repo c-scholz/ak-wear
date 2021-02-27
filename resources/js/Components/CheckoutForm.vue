fe2e1a5ab3908db5749ffa06c959fd29df76a73f
c-scholz <scholzcedric@web.de>
2021-02-27 12:29:24 +0100

    added checkout form, removed unused props from Pages

---
+++ resources/js/Components/CheckoutForm.vue
@@ -0,0 +1,46 @@
+<template>
+    <form id="" class="checkout-form">
+    </form>
+</template>
+
+<script>
+import numeral from 'numeral'
+import locales from 'numeral/locales'
+import { mapActions, mapGetters, mapState } from 'vuex'
+
+export default {
+    components: {
+    },
+    data() {
+        return {
+            
+        }
+    },
+    computed: {
+        ...mapState({
+            products: state => state.shop.products,
+            motifs: state => state.shop.motifs,
+            cart: state => state.cart.cart,
+        })
+    },
+    created() {
+    },
+    mounted() {
+
+        numeral.locale('de');
+    },
+    methods: {
+        ...mapActions('shop', [
+            'fetchMotifs',
+            'fetchProducts',
+        ]),
+        formatPrice(number) {
+            return numeral(number).format("0,0.00")
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Checkout.vue
+++ resources/js/Pages/Checkout.vue
@@ -2,18 +2,18 @@
   <div class="container">
     <div class="row justify-content-center mt-0">
       <div class="col-12 text-center p-0">
-        <order-form />
+        <checkout-form/>
       </div> <!-- Col-12 -->
     </div> <!-- Row -->
   </div> <!-- Container -->
 </template>
 
 <script>
-import OrderForm from '../Components/OrderForm'
+import CheckoutForm from '../Components/CheckoutForm'
 
 export default {
   components: {
-    OrderForm
+    CheckoutForm
   }
 }
 </script>
