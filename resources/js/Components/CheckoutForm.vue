76081fe95afa02ccfe3473b6c2a350403855fe77
VldmrGsn <vladgos@gmx.de>
2021-03-09 19:57:41 +0100

    edit checkout & add feature dialog modal

--- resources/js/Components/CheckoutForm.vue
+++ resources/js/Components/CheckoutForm.vue
@@ -34,6 +34,23 @@
                 />
                 <!-- <p class="form-text">Warenkorb</p> -->
 
+                <button type="button" class="btn btn-primary" @click="toggleModal">My Modal</button>
+
+                <div class="warenkorbLeer">
+                    Dein Warenkorb ist noch leer.
+                </div>
+                
+                <button class="warenkorbLeerButton1">
+                    <span class="warenkorbLeerText">
+                        <font-awesome-icon :icon="['fa', 'reply']" />&nbsp;&nbsp;Zurück zur Startseite
+                    </span>
+                </button>
+                <button class="warenkorbLeerButton2">
+                    <span class="warenkorbLeerText">
+                        Produkt hinzufügen&nbsp;&nbsp;<font-awesome-icon :icon="['fa', 'share']" />
+                    </span>
+                </button>
+
                 <section class="shoppingCartDesktop">
                     <div class="container">
                         <div class="row w-100">
@@ -43,10 +60,10 @@
                                     <tr>
                                         <th style="width:15%">Produkt</th>
                                         <th style="width:35%">Beschreibung</th>
-                                        <th style="width:10%;"></th>
-                                        <th style="width:15%;">Einzelpreis</th>
-                                        <th style="width:10%;">Menge</th>
-                                        <th style="width:15%;">Preis</th>
+                                        <th style="width:10%"></th>
+                                        <th style="width:15%">Einzelpreis</th>
+                                        <th style="width:10%">Menge</th>
+                                        <th style="width:15%">Preis</th>
                                     </tr>
                                 </thead>
                                     <tbody>
@@ -102,7 +119,7 @@
                                             <td data-th="Menge">
                                                 12 Stk.
                                             </td>
-                                            <td data-th="Menge">
+                                            <td data-th="Menge" class="preisAbstand">
                                                 407,88 EUR<br />
                                                 +17,50 EUR<br />
                                             </td>
@@ -175,7 +192,7 @@
                                             <td data-th="Menge">
                                                 25 Stk.
                                             </td>
-                                            <td data-th="Menge">
+                                            <td data-th="Menge" class="preisAbstand">
                                                 599,75 EUR<br />
                                                 +52,50 EUR<br />
                                                 +35,00 EUR
@@ -212,8 +229,43 @@
                                             <td data-th="Menge">
                                                 2 Stk.
                                             </td>
-                                            <td data-th="Menge">
+                                            <td data-th="Menge" class="preisAbstand">
                                                 35,98 EUR
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <table id="shoppingCart" class="table table-condensed" style="margin-bottom: 0;">
+                                    <thead>
+                                        <tr>
+                                            <th style="width:45%"></th>
+                                            <th style="width:25%"></th>
+                                            <th style="width:10%"></th>
+                                            <th style="width:15%;"></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <div class="input-group">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text">Rabattcode</div>
+                                                    </div>
+                                                    <input class="form-control" type="text" placeholder="Beispiel: XMAS2021">
+                                                </div>
+                                            </td>
+                                            <td></td>
+                                            <td class="text-right">
+                                                <span>Summe</span><br />
+                                                <span>Rabatt</span><br />
+                                                <span>Versand</span><br />
+                                                <span class="gesamtsummeText">Gesamt</span>
+                                            </td>
+                                            <td class="preisAbstand">    
+                                                <span>1.148,61 EUR</span><br />
+                                                <span>0,00 EUR</span><br />
+                                                <span>10,99 EUR</span><br />
+                                                <span class="gesamtsummeZahlen">1.158,61 EUR</span>
                                             </td>
                                         </tr>
                                     </tbody>
@@ -222,7 +274,6 @@
                         </div>
                     </div>
                 </section>
-
                 <section class="shoppingCartMobile">
                     <div class="warenkorbAbstand"></div>
                     <div class="row">
@@ -419,15 +470,38 @@
                             <p>35,98 EUR</p>
                         </div>
                     </div>
-                </section>
-
-                <div class="row d-flex align-items-end flex-column">
-                    <div class="gesamtsumme">
-                        <span>Gesamtsumme</span><br />
-                        <span>1.148,61 EUR</span>
+                    <hr class="mobileHr">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text">Rabattcode</div>
+                        </div>
+                        <input class="form-control" type="text" placeholder="Beispiel: XMAS2021">
                     </div>
-                </div>
-                
+                    <div class="row gesammtsumme">
+                        <div class="col-4 dreierReihe text-center noPaddingRight4Ever">
+                            <div class="tableHeading">
+                                Summe
+                            </div>
+                            <span>1.148,61 EUR</span>
+                        </div>
+                        <div class="col-4 dreierReihe text-center noPadding4Ever">
+                            <div class="tableHeading">
+                                Rabatt
+                            </div>
+                            <span>0,00 EUR</span>
+                        </div>
+                        <div class="col-4 dreierReihe text-center noPadding4Ever">
+                            <div class="tableHeading">
+                                Versand
+                            </div>
+                            <span>10,99 EUR</span>
+                        </div>
+                        <div class="col-12 dreierReihe text-center noPaddingLeft4Ever" style="font-size: 16px; margin-top: 30px;">
+                            <span style="font-family: 'Muli ExtraBold', sans-serif;">Gesamt</span> <span>1.158,61 EUR</span>
+                        </div>
+                    </div>
+                </section>
+               
                 <!-- ###################### CONTENT ###################### -->
                 <!-- ###################### MOBILE BUTTONS & FEHLER ###################### -->
                 <fieldset-footer 
@@ -514,6 +588,7 @@
 import SvgGradient from './SvgGradient'
 import FieldsetHead from './FieldsetHead'
 import FieldsetFooter from './FieldsetFooter'
+import DialogModal from './DialogModal'
 import numeral from 'numeral'
 import locales from 'numeral/locales'
 import { mapActions, mapGetters, mapState } from 'vuex'
@@ -522,7 +597,8 @@ export default {
     components: {
         SvgGradient,
         FieldsetHead,
-        FieldsetFooter
+        FieldsetFooter,
+        DialogModal
     },
     data() {
         return {
@@ -745,6 +821,49 @@ export default {
     }
 }
 
+.warenkorbLeer {
+    font-family: 'Muli ExtraBold', sans-serif;
+    font-size: 30px;
+    margin: 120px 0;
+}
+
+.warenkorbLeerButton1 { margin-right: 5px; }
+.warenkorbLeerButton2 { margin-left: 5px; }
+
+.warenkorbLeerButton1,
+.warenkorbLeerButton2 {
+    font-family: "Muli ExtraBold", sans-serif;
+    text-transform: uppercase;
+    font-size: 12px;
+    color: #fff;
+    background-color: #1A1A1A;
+    border: 1px solid #1A1A1A;
+    border-radius: 12px;
+    padding: 15px;
+    width: 205px;
+}
+
+.warenkorbLeerButton1:hover .warenkorbLeerText,
+.warenkorbLeerButton1:focus .warenkorbLeerText,
+.warenkorbLeerButton2:hover .warenkorbLeerText,
+.warenkorbLeerButton2:focus .warenkorbLeerText {
+    background-image: linear-gradient(to bottom right, #ffae27, #d93548);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+}
+
+.warenkorbLeerButton1:focus .fa-reply path, .warenkorbLeerButton1:hover .fa-reply path,
+.warenkorbLeerButton2:focus .fa-reply path, .warenkorbLeerButton2:hover .fa-reply path,
+.warenkorbLeerButton1:focus .fa-share path, .warenkorbLeerButton1:hover .fa-share path,
+.warenkorbLeerButton2:focus .fa-share path, .warenkorbLeerButton2:hover .fa-share path {
+    fill: url(#linear);
+}
+
+@media (max-width: 462px) {
+    .warenkorbLeerButton1 { margin-right: 0; }
+    .warenkorbLeerButton2 { margin: 15px 0 0 0; }
+}
+
 img.produkt {
     height: 80px;
     margin: 0 auto;
@@ -795,29 +914,15 @@ tbody tr td {
 
 thead tr th:nth-child(2) { text-align: left; }
 
-.gesamtsumme {
-    background: #1a1a1a;
-    border-radius: 12px;
-    color: #fff;
-    padding: 15px 35px 17px;
-    width: fit-content;
-    margin-right: 15px;
+.gesamtsummeText {
+    font-family: 'Muli ExtraBold', sans-serif;
+    display: block;
+    margin-top: 10px;
 }
 
-.gesamtsumme span:first-child {
-    font-family: "Muli ExtraBold", sans-serif;
-    text-transform: uppercase;
-    font-size: 12px;
-}
-
-.gesamtsumme span:last-child {
-    font-family: "Muli ExtraBold", sans-serif;
-    text-transform: uppercase;
-    font-size: 35px;
-    line-height: 40px;
-    background-image: linear-gradient(to bottom right, #ffae27, #d93548);
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
+.gesamtsummeZahlen {
+    display: block;
+    margin-top: 10px;
 }
 
 .shoppingCartMobile { display: none; }
@@ -836,6 +941,45 @@ thead tr th:nth-child(2) { text-align: left; }
     transition: all 0.25s;
 }
 
+.input-group {
+    width: 412px;
+    margin: 0 auto;
+}
+
+.input-group-prepend {
+    margin-right: 0;
+}
+
+.input-group input::placeholder {
+    color: #a6a6a6;
+}
+
+input.form-control {
+    border-left: 0;
+    border-color: #e6e6e6;
+    height: auto;
+}
+
+input.form-control:focus {
+    color: #1a1a1a;
+    border-color: #1a1a1a !important;
+}
+
+.input-group-text {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: "Muli ExtraBold", sans-serif;
+    font-size: 12px;
+    text-transform: uppercase;
+    background-color: transparent;
+    border: 2px solid #e6e6e6;
+    border-top-left-radius: 12px;
+    border-bottom-left-radius: 12px;
+    padding: 11px 17px 10px;
+    flex: auto;
+}
+
 .shoppingCartButton {
     color: #1a1a1a;
     height: 40px;
@@ -844,6 +988,17 @@ thead tr th:nth-child(2) { text-align: left; }
     border-radius: 12px;
     padding: 10px;
     transition: all 0.25s;
+}
+
+.preisAbstand {
+    text-align: right;
+    padding-right: 30px;
+}
+
+@media (max-width: 1199px) {
+    .input-group {
+        width: 354px;
+    }
 }
 
 @media (max-width: 991px) {
@@ -865,7 +1020,7 @@ thead tr th:nth-child(2) { text-align: left; }
     }
 
     .shoppingCartMobile h4,
-    .shoppingCartMobile span { 
+    .col-9 span { 
         font-family: 'Muli ExtraBold', sans-serif; 
     }
 
@@ -900,13 +1055,14 @@ thead tr th:nth-child(2) { text-align: left; }
         margin: 30px 15px;
     }
 
-    .gesamtsumme { margin-top: 30px; }
-}
+    .input-group {
+        width: 100%;
+        margin-bottom: 30px;
+        padding: 0 15px;
+    }
 
-@media (max-width: 575px) {
-    .gesamtsumme {
-        display: block;
-        margin: 30px auto 0;
+    .gesammtsumme {
+        margin-bottom: 15px;
     }
 }
 </style>
